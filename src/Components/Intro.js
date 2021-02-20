7de5a7fe7e9b5fa35e334b45d3f0d92854eb3c78
Pranaya Anargya <panargya@gmail.com>
2021-02-20 13:13:35 +1100

    added intro file

---
+++ src/Components/Intro.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Intro = () => {
+    return (
+        <div>
+            <h1>You are worth it</h1>
+            <p>An iceberg is comparable to a person's mind. It reveals only characteristics needed for social interactions and leaves an impression that it's depth is only till what you see. Much like an iceberg</p>
+            <p>The reality of the mind is revealed when we plunge deep beneath the water. Revealing a complex history that shaped it to become what it is today</p>
+        </div>
+    )
+}
+
+export default Intro
