367db562c2d937894d8a7464ca44feddcd6b17da
Pranaya Anargya <panargya@gmail.com>
2021-02-20 19:07:48 +1100

    finished content draft for peer review

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -6,6 +6,36 @@ const Intro = () => {
             <h1>You are worth it</h1>
             <p>An iceberg is comparable to a person's mind. It reveals only characteristics needed for social interactions and leaves an impression that it's depth is only till what you see. Much like an iceberg</p>
             <p>The reality of the mind is revealed when we plunge deep beneath the water. Revealing a complex history that shaped it to become what it is today</p>
+            <p>Mental Health disorders exist.</p>
+            <p>It is an experience that starts as early as  adolescents and has become the third leading case of death for adolescents []</p>
+            <p>For years, mental health disorders have been stigmatized by cultures and resulted in shaming by both community and family especially among asian communities[vox]</p>
+            <p>This has pushed sufferers of mental health disorder into hiding. In fear of being shamed.[vox]</p>
+            <p>Tough Love</p>
+            <p>Growing up in an asian family, the phrase 'toughen up' is oftenly used as a method of coping . It means to accept the trobles of life. [ABC]</p>
+            <p>It has long been seen that asian parents treat their child the harshest. As making a small mistake resulted in consequences or dishonor. While the consequences are at times cruel and traumatizing we've often ignored the intention of this act.</p>
+            <p>It is not under the name of a good intention but to preserve and respect traditions and those that have practiced before. </p>
+            <p>This moral practice has been passed down for generations and disobedience is seen as breaking tradition which can also indicate disloyalty to the family.</p>
+            <p>There are those that have it rough and some who have it worse. But that doesn't make your pain far lesser!</p>
+            <p>Eveyone experiences hardships in their lives no matter whho it is. Everyone fights their own batttles that we can't see. That includes those that struggle the most.</p>
+            <p>Despite invisible to the eye, we can still make an impact.</p>
+            <p>One's happiness is like a jug full of water, it is one's purpose to pour to the empty cups</p>
+            <p>Although subtle, these subtle acts can make large impacts on someone's life:</p>
+            <ul>
+                <li>be kind</li>
+                <li>reach out to people</li>
+                <li>spread positivity</li>
+            </ul>
+            <p>Life has no definite structure.</p>
+            <p>at times we find our hardships dragging us down and taking us steps back and it is okay. do what you must and take the necessary stapes to get through the day.</p>
+            <p>as tomorrow holds such better days.</p>
+            <p>if you find yourself in this situation , these are some steps that you can take:</p>
+            <ul>
+                <li>take care of your bod</li>
+                <li>draw only good aspects</li>
+                <li>be kind to self</li>
+            </ul>
+            <p>Taking thses steps are not easy when your mind is clouded. but by acting to it is worth the effort to a better life. as by acting on these steps on, you have just proven that....</p>
+            <p>...you are worth it</p>
         </div>
     )
 }
