a59d16a1e18d0ee8dbe14ab4fb136e00bed73484
Pranaya Anargya <panargya@gmail.com>
2021-02-24 20:54:30 +1100

    connected theme to cover and created global style

--- src/Components/CoverPage/Cover.js
+++ src/Components/CoverPage/Cover.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import theme from "../Utilities/Themes";
 
 const Cover = () => {
-    return (
-        <div>
-            <h1>Cover</h1>
-        </div>
-    )
-}
+	return (
+		<ThemeProvider theme={theme}>
+			<section>
+				<h1>You Are Worth It.</h1>
+			</section>
+		</ThemeProvider>
+	);
+};
 
-export default Cover
+export default Cover;
